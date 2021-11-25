fb80e2951aa5208aacaf251738f5c64bc8586835
Clément Martinez <clement.martinez@smarkets.com>
2021-11-25 17:26:44 +0000

    💩💩💩

--- db_interface.py
+++ db_interface.py
@@ -20,7 +20,8 @@ def execute_gg_command(cmd) -> int:
 
     db.GG(db.db_conn, (cmd.side_1, cmd.side_2, cmd.score_1, cmd.score_2, None, cmd.match_id))
     
-def execute_out_command(command: c.OutCommand) -> t.Set[str]:
+def execute_out_command(command) -> t.Set[str]:
     """
     Out the current player and do re-matching
     """
+    db.out(db.db_conn, command.user_id)
