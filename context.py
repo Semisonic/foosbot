a52f1641e6786aef253d9a6e8e6c66cdfc66ffc9
Dmitry Semionin <Dmitry.Semionin@smarkets.com>
2021-11-25 14:38:16 +0000

    more parsing

--- context.py
+++ context.py
@@ -1,6 +1,6 @@
 import typing as t
 
 class RequestContext:
-    def __init__(self, caller_id: str, user_ids: t.List[str]) -> None:
+    def __init__(self, caller_id: str, user_ids: t.Set[str]) -> None:
         self.caller_id = caller_id
         self.user_ids = user_ids
